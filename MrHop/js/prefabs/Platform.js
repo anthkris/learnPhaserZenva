33111b57a682b601b3173a89f482689ba790b240
anthkris09 <anthkris09@gmail.com>
2016-09-08 23:33:22 -0400

    Implement pool of platforms and pseudo-random platform generation

--- MrHop/js/prefabs/Platform.js
+++ MrHop/js/prefabs/Platform.js
@@ -1,19 +1,22 @@
 var MrHop = MrHop || {};
 
-MrHop.Platform = function(game, floorPool, numTiles, x, y) {
+MrHop.Platform = function(game, floorPool, numTiles, x, y, speed) {
   Phaser.Group.call(this, game);
   this.game = game;
   this.floorPool = floorPool;
   this.tileSize = 40;
   this.enableBody = true;
-  this.prepare(numTiles, x, y);
+  this.prepare(numTiles, x, y, speed);
  
 };
 
 MrHop.Platform.prototype = Object.create(Phaser.Group.prototype);
 MrHop.Platform.prototype.constructor = MrHop.Platform;
 //create new method in order to reposition dead sprites as well as new sprites
-MrHop.Platform.prototype.prepare = function(numTiles, x, y){
+//speed passed here so that dead sprites can be revived with new speed
+MrHop.Platform.prototype.prepare = function(numTiles, x, y, speed){
+  this.alive = true;
+  
   var i = 0;
   while(i < numTiles){
     
@@ -30,4 +33,22 @@ MrHop.Platform.prototype.prepare = function(numTiles, x, y){
   //set physics properties
   this.setAll('body.immovable', true);
   this.setAll('body.allowGravity', false);
+  this.setAll('body.velocity.x', speed);
+};
+
+MrHop.Platform.prototype.kill = function(){
+  this.alive = false;
+  //call the kill method on each individual sprite in the group
+  this.callAll('kill');
+  var sprites = [];
+  
+  //send each sprite back to floorPool
+  //using sprites array as an intermediary to prevent issue with modifying group while iterating through it.
+  this.forEach(function(tile){
+    sprites.push(tile);
+  });
+  
+  sprites.forEach(function(tile){
+     this.floorPool.add(tile);          
+  }, this);
 };
--- MrHop/js/states/Game.js
+++ MrHop/js/states/Game.js
@@ -6,6 +6,9 @@ MrHop.GameState = {
     //pool of floor sprites
     this.floorPool = this.add.group();
     
+    //pool of platforms
+    this.platformPool = this.add.group();
+    
     //gravity
     this.game.physics.arcade.gravity.y = 1000; 
     
@@ -17,12 +20,17 @@ MrHop.GameState = {
     
     //coin count
     this.myCoins = 0;
+    
+    //level speed
+    this.levelSpeed = 200;
   },
   create: function() {
-
+ 
     //hard-code first platform
-    this.platform = new MrHop.Platform(this.game, this.floorPool, 12, 0, 200);
-    this.add.existing(this.platform);
+    this.currentPlatform = new MrHop.Platform(this.game, this.floorPool, 12, 0, 200, -this.levelSpeed);
+    this.platformPool.add(this.currentPlatform);
+    
+    this.loadLevel();
     
     //create player
     this.player = this.add.sprite(50, 50, 'player');
@@ -34,18 +42,37 @@ MrHop.GameState = {
     this.player.body.setSize(38, 60, 0, 0);
     this.player.play('running');
   },   
-  update: function() {    
-    this.game.physics.arcade.collide(this.player, this.platform);
+  update: function() {
+    this.platformPool.forEachAlive(function(platform, index){
+      this.game.physics.arcade.collide(this.player, platform);
+      
+      //check if platform needs to be killed
+      if(platform.length && platform.children[platform.length - 1].right < 0) {
+        platform.kill();
+      }
+    }, this);
+    
+    if (this.player.body.touching.down) {
+      this.player.body.velocity.x = this.levelSpeed;
+    }
+    else {
+      this.player.body.velocity.x = 0;
+    }
     
     if(this.cursors.up.isDown || this.game.input.activePointer.isDown){
       this.playerJump();
     } else if (this.cursors.up.isUp || this.game.input.activePointer.isUp) {
       this.isJumping = false;
     }
+    
+    //if the last sprite in the platform group is showing, then create a new platform
+    if(this.currentPlatform.length && this.currentPlatform.children[this.currentPlatform.length - 1].right < this.game.world.width) {
+      this.createPlatform();
+    }
   },
   render: function(){
-    this.game.debug.body(this.player);
-    this.game.debug.bodyInfo(this.player, 0, 30);
+    //this.game.debug.body(this.player);
+    //this.game.debug.bodyInfo(this.player, 0, 30);
   },
   playerJump: function(){
     if(this.player.body.touching.down) {
@@ -65,5 +92,48 @@ MrHop.GameState = {
         this.jumpPeaked = true;
       }
     }
+  },
+  loadLevel: function() {
+    //this.currIndex = 0;
+    this.createPlatform();
+  },
+  createPlatform: function(){
+    var nextPlatformData = this.generateRandomPlatform();
+    
+     if (nextPlatformData) {
+       this.currentPlatform = this.platformPool.getFirstDead();
+       if (!this.currentPlatform) {
+         this.currentPlatform = new MrHop.Platform(this.game, this.floorPool, nextPlatformData.numTiles, 
+                                                 this.game.world.width + nextPlatformData.separation, nextPlatformData.y, -this.levelSpeed);
+       } else {
+         this.currentPlatform.prepare(nextPlatformData.numTiles, this.game.world.width + nextPlatformData.separation, 
+                                      nextPlatformData.y, -this.levelSpeed)
+       }
+       
+       this.platformPool.add(this.currentPlatform);
+       this.currIndex++;
+     }
+  },
+  generateRandomPlatform: function(){
+    var data = {};
+    //generate separation/ distance from prev platform
+    var minSeparation = 60;
+    var maxSeparation = 200;
+    data.separation = minSeparation + Math.random() * (maxSeparation - minSeparation);
+    
+    //y, in regards to the prev platform
+    var minDifY = -120;
+    var maxDifY = 120;
+    data.y = this.currentPlatform.children[0].y + minDifY + Math.random() * (maxDifY - minDifY);
+    //set absolute max and min for platforms to overwrite random gen, if necessary
+    data.y = Math.max(150, data.y);
+    data.y = Math.min(this.game.world.height - 50, data.y);
+    
+    //number of tiles
+    var minTiles = 1;
+    var maxTiles = 5;
+    data.numTiles = minTiles + Math.random() * (maxTiles - minTiles);
+    
+    return data;
   }
-};
+}
